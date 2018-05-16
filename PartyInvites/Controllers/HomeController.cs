ef27a73668448880be79dac0ad7623ac3a85c985
Harry Masidza <harrymas21@gmail.com>
2018-05-16 13:29:31 +0300

    first commit

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+using PartyInvites.Repo;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+
+            ViewBag.Greetings = hour < 12 ? "Good Morning World!" : "Good Afternoon World";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                //there's some validation error
+                return View("RsvpForm", guestResponse);
+            }
+        }
+
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
